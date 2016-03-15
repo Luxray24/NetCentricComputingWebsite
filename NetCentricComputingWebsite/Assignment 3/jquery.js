9b9bbc7087d042fba3f5cdc5107eeeda0f25a908
Mark William Swarner <luxray24_2010@yahoo.com>
2016-03-15 17:08:47 -0400

    Added assignment 3

---
+++ NetCentricComputingWebsite/Assignment 3/jquery.js
@@ -0,0 +1,52 @@
+var script = document.createElement('script');
+script.src = 'http://code.jquery.com/jquery-1.11.0.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+function hideButton0(){
+	$("#button0").hide();
+}
+
+function hideButton2(){
+	$("#button2").hide();
+}
+
+function hideButton3(){
+	$("#button3").hide();
+}
+
+function hideButton4(){
+	$("#button4").hide();
+}
+
+function hideButton6(){
+	$("#button6").hide();
+}
+
+function hideButton7(){
+	$("#button7").hide();
+}
+
+function hideButton9(){
+	$("#button9").hide();
+}
+
+function hideButton12(){
+	$("#button12").hide();
+}
+
+function hideButton13(){
+	$("#button13").hide();
+}
+
+function hideButton15(){
+	$("#button15").hide();
+}
+
+function win(){
+	$("#win").show();
+}
+
+function lose(){
+	$("button").hide();
+}
